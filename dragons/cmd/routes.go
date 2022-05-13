c6b321520ad3a927c585c3a992930a2c2ce3d4b5
augustine <augustino0890@gmail.com>
2022-05-13 14:35:30 +0900

    dragons: use gorilla mux routes

--- dragons/cmd/routes.go
+++ dragons/cmd/routes.go
@@ -1,15 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func (app *Application) routes() *http.ServeMux {
-	mux := http.NewServeMux()
+	"github.com/gorilla/mux"
+)
 
-	mux.HandleFunc("/s3/list", app.listBuckets)
-	mux.HandleFunc("/s3/bucket/list", app.listItems)
-	mux.HandleFunc("/s3/bucket", app.deleteBucket)
-	mux.HandleFunc("/s3/bucket", app.queryBucket)
-	mux.HandleFunc("/s3/create", app.createBucket)
+func (app *Application) routes() *mux.Router {
+	mux := mux.NewRouter()
+
+	mux.HandleFunc("/s3/buckets", app.listBuckets)
+	mux.HandleFunc("/s3/bucket/items", app.listItems)
+	mux.HandleFunc("/s3/bucket", app.createBucket).Methods(http.MethodPost)
+	mux.HandleFunc("/s3/bucket", app.queryBucket).Methods(http.MethodGet)
+	mux.HandleFunc("/s3/bucket", app.deleteBucket).Methods(http.MethodDelete)
 
 	return mux
 }
--- dragons/go.mod
+++ dragons/go.mod
@@ -4,4 +4,7 @@ go 1.17
 
 require github.com/aws/aws-sdk-go v1.43.41
 
-require github.com/jmespath/go-jmespath v0.4.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
